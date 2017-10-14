7678aabc116fa9ef0224255afae0d89a35919c02
Tim <timdonlan@gmail.com>
2017-10-13 21:54:03 -0400

    added player avatars

--- Assets/Scripts/CharacterSceneController.cs
+++ Assets/Scripts/CharacterSceneController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Linq;
 
 public class CharacterSceneController : MonoBehaviour
 {
@@ -10,17 +11,33 @@ public class CharacterSceneController : MonoBehaviour
 	public GameData gameData;
 	public Text StatsText;
 
+	public Image playerImg;
+
+	private int playerSpriteIndex = 0;
+	private List<Sprite> PlayerSpriteList;
+
 	// Use this for initialization
 	void Start ()
 	{
 		gameData = GameObject.FindObjectOfType<GameData> ();
 		SetStatsText ();
+		loadResources ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		
+	}
+
+	private void loadResources ()
+	{
+		PlayerSpriteList = Resources.LoadAll ("Player1", typeof(Sprite)).Cast<Sprite> ().ToList ();
+		/*
+		foreach (var s in playerSprites) {
+			PlayerSpriteList.Add (s);
+		}
+		*/
 	}
 
 	private void SetStatsText ()
@@ -43,5 +60,14 @@ public class CharacterSceneController : MonoBehaviour
 	public void BackButton ()
 	{
 		SceneManager.LoadScene ("StartScene", LoadSceneMode.Single);
+	}
+
+	public void NextImg ()
+	{
+		playerSpriteIndex++;
+		if (playerSpriteIndex >= PlayerSpriteList.Count) {
+			playerSpriteIndex = 0;
+		}
+		playerImg.sprite = PlayerSpriteList [playerSpriteIndex];
 	}
 }
